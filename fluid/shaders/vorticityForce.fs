f6ce9c385b074c0a56e6605e22aaffc8f0324c0f
Ilyas Tazitdinov <itazitdinov@acumatica.com>
2019-06-13 05:04:34 +0300

    vorticity fix

--- fluid/shaders/vorticityForce.fs
+++ fluid/shaders/vorticityForce.fs
@@ -19,16 +19,10 @@ void main(){
     float t = texture(vorticity, uv_t).x;
     float b = texture(vorticity, uv_b).x;
     float c = texture(vorticity, uv).x;
-    // vec2 force = 0.5 * vec2(abs(t) - abs(b), abs(r) - abs(l));
-    // float lengthRSqr = 1.0 / max(2.4414e-4, dot(force,force));
-    // force *= lengthRSqr * dxscale * vec2(1,-1);
-    // vec2 vel = texture(velocity, uv).xy;
-    // color = vec3(vel + force * dt, 0.0);
 
     vec2 force = 0.5 * vec2(abs(t) - abs(b), abs(r) - abs(l));
-    force /= length(force) + 0.0001;
-    force *= dxscale * c;
-    force.y *= -1.0;
+    force /= max(length(force), 2.4414e-4);
+    force *= dxscale * c * vec2(1,-1);
     vec2 vel = texture(velocity, uv).xy;
     color = vec3(vel + force * dt, 0.0);
 }
